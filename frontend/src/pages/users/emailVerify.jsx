ebb019b1cedd607b2ac578ecdedcd0966c9ca70b
sinaboorj <127826905+sinaboorj@users.noreply.github.com>
2024-04-25 23:45:02 +0330

    register 4030206

--- frontend/src/pages/users/emailVerify.jsx
+++ frontend/src/pages/users/emailVerify.jsx
@@ -4,35 +4,30 @@ import success from '/sysImage/success.png'
 import error from '/sysImage/warningSmall.png'
 import { Link, useParams } from 'react-router-dom'
 import axios from 'axios'
+import { UserContext } from '../../context/userContext'
 
 const EmailVerify = () => {
-    const [validUrl, setValidUrl] = useState(true)
     const [validationMsg, setValidationMsg] = useState('')
-    const [validStatusMsg, setValidStatusMsg] = useState(false)
     const { userID, token } = useParams();
     const [isLoading, setIsLoading] = useState(true);
-
+    const {setHidden } = useContext(UserContext);
 
     useEffect(() => {
         const verifyEmailUrl = async () => {
             try {
                 const url = `http://localhost:5500/api/${userID}/mail-verification/${token}`//localhost:متغيير 
                 const result = await axios.put(url)
-                if (result.data.verifyStatue) {     // تاييد ايميل 
-                    setValidStatusMsg(true)
-                    setValidationMsg(result.data.msg)
-                    setValidUrl(true)
-                } else {                          // ايميل قبلا تاييد شده است     
-                    setValidStatusMsg(false)
-                    setValidationMsg(result.data.msg)
-                    setValidUrl(true)
+               
+                if (result) {        // تاييد ايميل 
+                    setValidationMsg(result.data)
                 }
-            } catch (error) {                      // خطاي تاييد ايميل يا خطاي لينك
-                setValidUrl(false)
-                setValidationMsg(`' Invalivd Link ' or ' Interner Error '`)
+    
+            } catch (error) {        // خطاي تاييد ايميل يا خطاي لينك
+                setValidationMsg(error.response.data)
             }
             setIsLoading(false)
         }
+        setHidden(true)
         verifyEmailUrl()
     }, [userID, token])
 
@@ -40,48 +35,38 @@ const EmailVerify = () => {
         <>
             {isLoading && (
                 <div className='contaner-verify'>
-                    <h4 style={{ color: 'blue', textAlign: 'center' }}>Waiting... <img src="/sysImage/addUser.gif" width={100} height={100} alt="Loading user" /></h4>
+                    <h4 style={{ color: '#b7b2b2', textAlign: 'center' }}>Waiting... <img src="/sysImage/loading.gif" width={100} height={100} alt="Loading user" /></h4>
                 </div>
-            )
-            }
-            {
-                validUrl && !isLoading &&
-                (
-                    <div className='contaner-verify'>
-                        <img src={success} alt="Verify-img" className='verification-img' />
-                        <h5 style={{ color: validStatusMsg ? 'black' : 'red', textAlign: 'center' }}>{validationMsg}</h5>
-                        <Link to={'/api/login:signup'} style={{
-                            ...validStatusMsg ? {
-                                background: '#15a763',
-                                padding: '5px 35px',
-                                marginTop: '15px',
-                                textDecoration: 'none',
-                                color: 'black',
-                                fontWeight: '600',
-                                borderRadius: '15px',
-                            } : {
-                                padding: '5px 35px',
-                                marginTop: '15px',
-                                textDecoration: 'none',
-                                color: 'black',
-                                fontWeight: '600',
-                                borderRadius: '15px',
-                                border: 'solid 2px green',
-                            }
-                        }} >
-                            Login
-                        </Link>
-                    </div>
-                )}
-            {!validUrl && !isLoading && (
+            )}
+
+            {validationMsg.verification && validationMsg.title === 'Successful' && (
+                <div className='contaner-verify'>
+                    <img src={success} alt="Verify-img" className='verification-img' />
+                    <h5 style={{ color: '#b7b2b2', textAlign: 'center' }}>{validationMsg.msg}</h5>
+                    <Link to={'/api/login:signup'} className='verifylogin'>
+                        Login
+                    </Link>
+                </div>
+            )}
+
+
+            {validationMsg.verification && validationMsg.title === 'Error' && (
+                <div className='contaner-verify'>
+                    <img src={success} alt="Verify-img" className='verification-img' />
+                    <h5 style={{ color: 'red', textAlign: 'center' }}>{validationMsg.msg}</h5>
+                    <Link to={'/api/login:signup'} className='verifylogin'>
+                        Login
+                    </Link>
+                </div>
+            )}
+           
+            {!validationMsg.verification && validationMsg.title === 'Error' && (
                 <div className='contaner-verify'>
                     <img src={error} alt="Verify-img" className='verification-img' />
-                    <h5 style={{ color: 'black', textAlign: 'center' }}>Verification Error</h5>
-                    <h6 style={{ color: 'black' }}>{validationMsg}</h6>
+                    <h5 style={{ color: 'red', textAlign: 'center' }}>{validationMsg.title}</h5>
+                    <h6 style={{ color: '#b7b2b2' }}>{validationMsg.msg}</h6>
                 </div>
-            )
-                        
-            }
+            )}
         </>
     )
 }
